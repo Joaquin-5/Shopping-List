acf1ce9aaa9e47e07fa88c0008146e7916e7c013
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-09-28 16:31:14 -0500

    endpoints

---
+++ shopping-back/src/categories/categories.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './entities/category.entity';
+
+@Module({
+  controllers: [CategoriesController],
+  providers: [CategoriesService],
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([
+      {
+        name: Category.name,
+        schema: CategorySchema,
+      },
+    ]),
+  ],
+})
+export class CategoriesModule {}
---
+++ shopping-back/src/categories/categories.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectModel(Category.name)
+    private categoryModel: Model<Category>,
+  ) {}
+
+  create(createCategoryDto: CreateCategoryDto) {
+    return 'This action adds a new category';
+  }
+
+  findAll() {
+    return this.categoryModel.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
