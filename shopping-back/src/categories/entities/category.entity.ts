1f260a2b4df293ae0469161a0593c8a46cd59bd8
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-09-30 13:49:05 -0500

    extends del document mongoose

--- shopping-back/src/categories/entities/category.entity.ts
+++ shopping-back/src/categories/entities/category.entity.ts
@@ -1,7 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 @Schema()
-export class Category {
+export class Category extends Document {
   @Prop({
     required: true,
     unique: true,
