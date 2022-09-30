92e5f44bb321484f0d8c8bc9e14f4141a63abd65
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-09-30 13:49:39 -0500

    Se agrego mongoose al modulo

--- shopping-back/src/items/items.module.ts
+++ shopping-back/src/items/items.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Item, ItemSchema } from './entities/item.entity';
 
 @Module({
   controllers: [ItemsController],
-  providers: [ItemsService]
+  providers: [ItemsService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Item.name,
+        schema: ItemSchema,
+      },
+    ]),
+  ],
 })
 export class ItemsModule {}
