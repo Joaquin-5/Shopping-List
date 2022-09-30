5b1a7b60b81fa230262fac4e27535baeb6c7165c
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-09-30 13:49:49 -0500

    Entidad item

--- shopping-back/src/items/entities/item.entity.ts
+++ shopping-back/src/items/entities/item.entity.ts
@@ -1,1 +1,27 @@
-export class Item {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Category } from '../../categories/entities/category.entity';
+
+// User story: I can add a new item with name, category, note, and image.
+@Schema()
+export class Item extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  name: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Category.name,
+  })
+  category: Category;
+
+  @Prop()
+  note: string;
+
+  @Prop()
+  image: string;
+}
+
+export const ItemSchema = SchemaFactory.createForClass(Item);
