9642b7280308d68e4062f0528b73bc939b17dce0
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-10-14 14:51:15 -0500

    Sidebar recibe components hijos

--- Shopping-Front/src/components/sideBar/SideBar.tsx
+++ Shopping-Front/src/components/sideBar/SideBar.tsx
@@ -5,57 +5,70 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import Badge from "@mui/material/Badge";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../../store";
-import { changeState } from "../../store/ui";
 import "./sidebar.style.css";
+import { Box } from "@mui/material";
 
-export const SideBar = () => {
-  const dispatch = useDispatch();
+interface Props {
+  children: React.ReactNode;
+}
+
+export const SideBar: React.FC<Props> = ({ children }) => {
   const navigate = useNavigate();
-  const { showCart } = useSelector((state: RootState) => state.ui);
   const { items } = useSelector((state: RootState) => state.cart);
 
   return (
     <>
-      <div className="container">
-        <img src="logo.svg"></img>
-        <div className="button-container">
-          <Tooltip title="items">
-            <IconButton className="icon-button" sx={{ borderRadius: "5px" }} onClick={() => navigate("/")}>
-              <ListIcon sx={{ fontSize: "2rem" }} />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="history">
-            <IconButton className="icon-button" sx={{ borderRadius: "5px" }} onClick={() => navigate("/history")}>
-              <HistoryIcon sx={{ fontSize: "2rem" }} />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="statistics">
-            <IconButton className="icon-button" sx={{ borderRadius: "5px" }}>
-              <QueryStatsIcon sx={{ fontSize: "2rem" }} />
-            </IconButton>
-          </Tooltip>
-        </div>
-        <IconButton
-          sx={{
-            backgroundColor: "#F9A109",
-            color: "#FFFFFF",
-            "&:hover": { opacity: 1, backgroundColor: "#F9A109" },
-          }}
-          href="#shopping-list"
-        >
-          <Badge
-            badgeContent={items
-              .map((category) => category.items.length)
-              .reduce((a, b) => a + b, 0)}
-            color="error"
+      <Box display="flex" maxWidth={"100vw"} sx={{ overflow: "hidden" }}>
+        <div className="container">
+          <img src="logo.svg"></img>
+          <div className="button-container">
+            <Tooltip title="items">
+              <IconButton
+                className="icon-button"
+                sx={{ borderRadius: "5px" }}
+                onClick={() => navigate("/")}
+              >
+                <ListIcon sx={{ fontSize: "2rem" }} />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="history">
+              <IconButton
+                className="icon-button"
+                sx={{ borderRadius: "5px" }}
+                onClick={() => navigate("/history")}
+              >
+                <HistoryIcon sx={{ fontSize: "2rem" }} />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="statistics">
+              <IconButton className="icon-button" sx={{ borderRadius: "5px" }}>
+                <QueryStatsIcon sx={{ fontSize: "2rem" }} />
+              </IconButton>
+            </Tooltip>
+          </div>
+          <IconButton
+            sx={{
+              backgroundColor: "#F9A109",
+              color: "#FFFFFF",
+              "&:hover": { opacity: 1, backgroundColor: "#F9A109" },
+            }}
+            href="#shopping-list"
           >
-            <ShoppingCartOutlinedIcon />
-          </Badge>
-        </IconButton>
-      </div>
+            <Badge
+              badgeContent={items
+                .map((category) => category.items.length)
+                .reduce((a, b) => a + b, 0)}
+              color="error"
+            >
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </IconButton>
+        </div>
+        {children}
+      </Box>
     </>
   );
 };
