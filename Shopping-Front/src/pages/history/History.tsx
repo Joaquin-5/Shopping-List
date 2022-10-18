0f16668f218e5ca6da876c64a9404a30f27beeac
= <=>
2022-10-18 00:33:45 -0300

    Hystory page

--- Shopping-Front/src/pages/history/History.tsx
+++ Shopping-Front/src/pages/history/History.tsx
@@ -9,20 +9,64 @@ import { Cart } from "../../components/cart";
 export const HistoryPage = () => {
   return (
     <div className="history-container">
-      <div className="orders-container">
-        <h1>Shopping history</h1>
-        <p className="fecha-mes">August 2020</p>
-        <div className="order-container">
-          <h2 className="titulo-pedido">Grocery List</h2>
-          <div className="datos_pedido-container">
-            <div className="fecha-compra-container">
-              <CalendarMonthIcon />
-              <p className="fecha-compra">Mon 27.8.2020</p>
+      <div className="content-container">
+        <h1 className="titulo-principal-history">Shopping history</h1>
+        <div className="orders-container">
+          <p className="fecha-mes">August 2020</p>
+          <div className="order-container">
+            <h2 className="titulo-pedido">Grocery List</h2>
+            <div className="datos_pedido-container">
+              <div className="fecha-compra-container">
+                <CalendarMonthIcon />
+                <p className="fecha-compra">Mon 27.8.2020</p>
+              </div>
+              <p className="estado estado-completed">completed</p>
+              <IconButton className="ver-detalle" sx={{ color: "#F9A109" }}>
+                <ArrowForwardIosIcon />
+              </IconButton>
             </div>
-            <p className="estado estado-completed">completed</p>
-            <IconButton className="ver-detalle" sx={{color: '#F9A109'}}>
-              <ArrowForwardIosIcon />
-            </IconButton>
+          </div>
+          <div className="order-container">
+            <h2 className="titulo-pedido">Grocery List</h2>
+            <div className="datos_pedido-container">
+              <div className="fecha-compra-container">
+                <CalendarMonthIcon />
+                <p className="fecha-compra">Mon 27.8.2020</p>
+              </div>
+              <p className="estado estado-completed">completed</p>
+              <IconButton className="ver-detalle" sx={{ color: "#F9A109" }}>
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </div>
+          </div>
+        </div>
+        <div className="orders-container">
+          <p className="fecha-mes">August 2020</p>
+          <div className="order-container">
+            <h2 className="titulo-pedido">Grocery List</h2>
+            <div className="datos_pedido-container">
+              <div className="fecha-compra-container">
+                <CalendarMonthIcon />
+                <p className="fecha-compra">Mon 27.8.2020</p>
+              </div>
+              <p className="estado estado-completed">completed</p>
+              <IconButton className="ver-detalle" sx={{ color: "#F9A109" }}>
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </div>
+          </div>
+          <div className="order-container">
+            <h2 className="titulo-pedido">Grocery List</h2>
+            <div className="datos_pedido-container">
+              <div className="fecha-compra-container">
+                <CalendarMonthIcon />
+                <p className="fecha-compra">Mon 27.8.2020</p>
+              </div>
+              <p className="estado estado-cancelled">cancelled</p>
+              <IconButton className="ver-detalle" sx={{ color: "#F9A109" }}>
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </div>
           </div>
         </div>
       </div>
@@ -31,25 +75,4 @@ export const HistoryPage = () => {
   );
 };
 
-{
-  /* <div className="header">
-        <h1 className="titulo-principal">
-          <span className="span">Shoppingify</span> allows you take your
-          shopping list wherever you go
-        </h1>
-        <div>
-          <TextField
-            label="With normal TextField"
-            id="outlined-start-adornment"
-            sx={{ m: 1, width: "25ch" }}
-            InputProps={{
-              startAdornment: (
-                <Icon>
-                  <SearchIcon />
-                </Icon>
-              ),
-            }}
-          />
-        </div>
-      </div> */
-}
+
